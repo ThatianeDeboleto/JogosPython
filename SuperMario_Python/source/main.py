dd84d802827443ab73ef1a3e37696e17f7f2ef36
Thatiane Deboleto <drathatianenimtzz@icloud.com>
2022-01-22 11:55:06 -0300

    add jogo mario

---
+++ SuperMario_Python/source/main.py
@@ -0,0 +1,14 @@
+import pygame as pg
+from . import setup, tools
+from . import constants as c
+from .states import main_menu, load_screen, level
+
+def main():
+    game = tools.Control()
+    state_dict = {c.MAIN_MENU: main_menu.Menu(),
+                  c.LOAD_SCREEN: load_screen.LoadScreen(),
+                  c.LEVEL: level.Level(),
+                  c.GAME_OVER: load_screen.GameOver(),
+                  c.TIME_OUT: load_screen.TimeOut()}
+    game.setup_states(state_dict, c.MAIN_MENU)
+    game.main()
